5a6dd7f223e1adc66486b99e2569aec897a3d21a
Tyler <tylerxwebber@gmail.com>
2021-03-04 19:47:58 -0600

    rough draft 1

--- index.js
+++ index.js
@@ -4,25 +4,42 @@ document.addEventListener('DOMContentLoaded', function() {
     var addButton = document.getElementById("add-button");
     var todoCount = 0;
     var addTodo = function() {
-      var todoCol = document.createElement("div");
-      todoCol.setAttribute("class", "col-xs-12 todo");
+      var todoCol = document.createElement('div');
+      todoCol.setAttribute('class', 'col-xs-12 todo');
       var todoRow = document.createElement('div');
-      todoRow.setAttribute("class", "row");
-      var h5 = document.createElement("h5");
-      h5.setAttribute("class", "col-xs-8");
+      todoRow.setAttribute('class', 'row');
+      var removeButton = document.createElement('button');
+      removeButton.setAttribute('class','btn btn-danger remove-button');
+      removeButton.innerHTML = "REMOVE";
+
+      removeButton.onclick = function() {
+        var child = this.parentNode.parentNode;
+        todoList.removeChild(child);
+        todoCount++;
+      };
+
+      var h5 = document.createElement('h5');
+      h5.setAttribute('class', 'col-xs-8');
       h5.innerHTML = todoInput.value;
       todoRow.appendChild(h5);
+      todoRow.appendChild(removeButton);
       todoCol.appendChild(todoRow);
       todoList.appendChild(todoCol);
     };
 
-    addButton.addEventListener("click", function() {
+    var checkThenAddToDo = function () {
       if (todoCount < 10 && todoInput.value !== "") {
         addTodo();
         todoCount++;
         todoInput.value = "";   
       }
+    }
+
+    addButton.addEventListener("click", checkThenAddToDo);
+
+    todoInput.addEventListener("keyup", function (event) {
+      if (event.key === "Enter") {
+        checkThenAddToDo();
+      }
     })
-
-
   });
