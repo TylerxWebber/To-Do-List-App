42fc9f4d0e48c58aef8d5b9c6fd74d059a38271a
Tyler <tylerxwebber@gmail.com>
2021-03-04 19:08:28 -0600

    Add task function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    var todoList = document.getElementById("todo-list");
+    var todoInput = document.getElementById("todo-input");
+    var addButton = document.getElementById("add-button");
+    var todoCount = 0;
+    var addTodo = function() {
+      var todoCol = document.createElement("div");
+      todoCol.setAttribute("class", "col-xs-12 todo");
+      var todoRow = document.createElement('div');
+      todoRow.setAttribute("class", "row");
+      var h5 = document.createElement("h5");
+      h5.setAttribute("class", "col-xs-8");
+      h5.innerHTML = todoInput.value;
+      todoRow.appendChild(h5);
+      todoCol.appendChild(todoRow);
+      todoList.appendChild(todoCol);
+    };
+
+    addButton.addEventListener("click", function() {
+      if (todoCount < 10 && todoInput.value !== "") {
+        addTodo();
+        todoCount++;
+        todoInput.value = "";   
+      }
+    })
 
 
   });
